50ba74188e18c6838a9f044c9b92a22d421cd220
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 12:51:53 +0800

    inside the function merge sort, create an empty list

--- Merge Sort.py
+++ Merge Sort.py
@@ -10,7 +10,10 @@ print("[26, 22, 91, 67, 15, 49, 59, 32, 2, 17]")
 print("")
 
 def merge_sort(a,b):
-    pass
+    sorted_list = []
+    len_a = len(a)
+    len_b = len(b)
+    i = j = 0
 
 if __name__ == '__main__':
     a = [26, 22, 91, 67, 15]
