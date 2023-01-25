211e95aa6e014fb059760ffa1da0368a3f3239d6
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 18:03:11 +0800

    make a def partition and quick sort, then in the main i have listed the number list and print it.

--- Quick Sort.py
+++ Quick Sort.py
@@ -8,3 +8,14 @@ print("")
 
 print("[26, 22, 91, 67, 15, 49, 59, 32, 2, 17]")
 print("")
+
+def partition(elements):
+    pass
+
+def quick_sort(elements):
+    partition(elements)
+
+if __name__ == '__main__':
+    elements = [26, 22, 91, 67, 15, 49, 59, 32, 2, 17]
+    quick_sort(elements)
+    print(elements)
