a2b097d333acc0e65f23906571f053b788b9c98c
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 11:57:01 +0800

    making another loop inside the loop that define the number if it is greater than the nums

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -8,6 +8,8 @@ print("")
 
 def sort(nums):
     for i in range(len(nums)-1, 0, -1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
 
 
 nums = [26, 22, 91, 67, 15, 49, 59, 32, 2, 17]
