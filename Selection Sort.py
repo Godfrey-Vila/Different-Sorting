11f37fca17af447b7502d624835dc4b3a4bab152
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 11:27:47 +0800

    Making the preparation for the number list and print

--- Selection Sort.py
+++ Selection Sort.py
@@ -4,3 +4,10 @@ print("******** SECTION BSCOE 2-2 ***********")
 print("******* Sir Danilo Madrigalejos ******")
 
 print("Selection Sort")
+
+def sort(nums):
+
+nums = [26, 22, 91, 67, 15, 49, 59, 32, 2, 17]
+sort(nums)
+
+print(nums)
