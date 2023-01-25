9c1821267c9562e59cbd9ff72f1fa63937042d93
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 11:35:35 +0800

    inside the loop, next is creating for if statement for nums inside j

--- Selection Sort.py
+++ Selection Sort.py
@@ -6,9 +6,12 @@ print("******* Sir Danilo Madrigalejos ******")
 print("Selection Sort")
 
 def sort(nums):
-    for i in range(9):
+    for i in range(26):
         minpos = i
         for j in range (i,10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
 
 
 nums = [26, 22, 91, 67, 15, 49, 59, 32, 2, 17]
