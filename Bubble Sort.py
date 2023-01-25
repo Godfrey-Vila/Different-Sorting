50069653c4e2f72617ff29a097e8cd3980e4e701
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 11:59:00 +0800

    making an if statement iside the loop that swap the values by determining the values in the outer loop

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -10,6 +10,9 @@ def sort(nums):
     for i in range(len(nums)-1, 0, -1):
         for j in range(i):
             if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums [j+1] = temp
 
 
 nums = [26, 22, 91, 67, 15, 49, 59, 32, 2, 17]
