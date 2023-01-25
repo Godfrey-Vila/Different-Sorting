b04c5ba4b2aeb6cd0250f6587f5d6c447bf092c6
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 12:20:57 +0800

    make a loop with arange of 1 and a length for the element

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -6,4 +6,9 @@ print("******* Sir Danilo Madrigalejos ******")
 print("Insertion Sort")
 print("")
 
+print("[26, 22, 91, 67, 15, 49, 59, 32, 2, 17]")
+print("")
+
 def insertion_sort(elements):
+    for i in range(1, len(elements)):
+        anchor = elements[i]
