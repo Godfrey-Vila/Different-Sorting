876836c7245875bc57322b6684e2d699f1f5ead5
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 14:02:17 +0800

    printing name and title Quick sort

---
+++ Quick Sort.py
@@ -0,0 +1,10 @@
+print("********** PROGRAMMED BY:*************")
+print("********** GODFREY VILA **************")
+print("******** SECTION BSCOE 2-2 ***********")
+print("******* Sir Danilo Madrigalejos ******")
+
+print("Merge Sort")
+print("")
+
+print("[26, 22, 91, 67, 15, 49, 59, 32, 2, 17]")
+print("")
