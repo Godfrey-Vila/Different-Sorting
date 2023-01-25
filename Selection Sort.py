c4b575fc0ac8aa4d00222b4e461e764a626715ae
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 11:23:47 +0800

    Printing names and title slection sort

---
+++ Selection Sort.py
@@ -0,0 +1,6 @@
+print("********** PROGRAMMED BY:*************")
+print("********** GODFREY VILA **************")
+print("******** SECTION BSCOE 2-2 ***********")
+print("******* Sir Danilo Madrigalejos ******")
+
+print("Selection Sort")
