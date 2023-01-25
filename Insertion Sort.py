7a30f064401392ed7c4ca38604265ceccfb87777
Godfrey-Vila <vila.godfreyc@gmail.com>
2023-01-25 12:16:00 +0800

    printing name and title insertion sort

---
+++ Insertion Sort.py
@@ -0,0 +1,7 @@
+print("********** PROGRAMMED BY:*************")
+print("********** GODFREY VILA **************")
+print("******** SECTION BSCOE 2-2 ***********")
+print("******* Sir Danilo Madrigalejos ******")
+
+print("Insertion Sort")
+print("")
